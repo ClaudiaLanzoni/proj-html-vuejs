9b9aad411eccf8ad035b5c3f4a9011e6d1b86c34
ClaudiaLanzoni <claudia.lanzoni1991@gmail.com>
2021-10-06 16:40:24 +0200

    cardjumbotron creation and linking to parent main, first attempt printing it on screen

---
+++ src/components/CardJumbotron.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+
+        <div class="topic_container">
+            <img :src="topic.imgBgPath">
+            <div>{{topic.topic}}</div>
+            <div>{{topic.title}}</div>
+        
+       <!--
+           <div :style="{ 'background-image': `url(${element.src})` }"></div>
+           <img :src="element.src">
+            <div v-bind:style="{ 'background-image': 'url(../assets/images/img/blog-46.jpg)' }"></div>
+        -->
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name : 'CardJumbotron',
+    props : ['topic'],
+    data : function () {
+        return {
+        
+    }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+@import "../style/variables.scss";
+
+.topic_container {
+    width: 200px;
+    height: 200px;
+    border: 3px solid black;
+
+    img {
+        width: 70%;
+        height: 50%;
+    }
+}
+
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,16 +1,37 @@
 <template>
   <div>
     
-    
+    <CardJumbotron v-for="(element, index) in topicCard" :key="index" :topic="element"/>
+
+
   </div>
 </template>
 
 <script>
+import CardJumbotron from './CardJumbotron.vue';
+
 export default {
   name: 'Main',
   props: {
-    msg: String
+
+  },
+  components: {
+    CardJumbotron,
+  },
+  data : function () {
+    return {
+      topicCard : [{
+
+        imgBgPath : require('../assets/images/img/blog-46.jpg'),
+        topic : 'photography',
+        title : 'how to take better concert pictures in 30 seconds'
+      }]
+        
+        
+
+    
   }
+}
 }
 </script>
 
