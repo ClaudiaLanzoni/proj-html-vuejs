64c398e171a6ca42d3857ea65db704c9e60b958b
ClaudiaLanzoni <claudia.lanzoni1991@gmail.com>
2021-10-07 14:26:00 +0200

    Footer basic setting

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,12 +1,52 @@
 <template>
     <div>
+        <div class="footer_main_specifications_wrap">
+            <div class="container">
+                <div class="row">
+                    <div class="col-4">
 
+                    </div>
+
+                    <div class="col-3">
+
+                    </div>
+
+                    <div class="col-3">
+
+                    </div>
+
+                    <div class="col-2">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="footer_minor_specifications_wrap">
+            <div class="container">
+                <div class="row">
+                    <div class="col-2">
+
+                    </div>
+
+                    <div class="col-2">
+
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
     name : 'Footer',
+    props: {
+
+    },
+    components: {
+    
+  },
 }
 </script>
 
